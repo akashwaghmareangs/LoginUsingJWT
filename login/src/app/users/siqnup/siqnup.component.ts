d87fe45743fce70e96f0df687b582f2113424527
akash <akashwaghmareangs@gmail.com>
2025-01-12 15:43:20 +0530

    third

--- login/src/app/users/siqnup/siqnup.component.ts
+++ login/src/app/users/siqnup/siqnup.component.ts
@@ -1,29 +1,37 @@
+import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
-import {FormsModule} from '@angular/forms'
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-siqnup',
-  imports: [ReactiveFormsModule,FormsModule],
+  imports: [ReactiveFormsModule, FormsModule,NgIf],
   templateUrl: './siqnup.component.html',
-  styleUrl: './siqnup.component.css'
+  styleUrl: './siqnup.component.css',
 })
 export class SiqnupComponent implements OnInit {
-siqnupSubmit() {
-throw new Error('Method not implemented.');
-}
+  errmsg: any;
+  errmsgshow = false;
 
-
-  constructor(){
-
-  }
+  constructor() {}
   siqnupForm = new FormGroup({
-    name:new FormControl('',[Validators.required]),
-    email:new FormControl('',[Validators.required]),
-    password:new FormControl('',[Validators.required])
-  })
-  ngOnInit(): void {
-  
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+  ngOnInit(): void {}
+  siqnupSubmit() {
+    if (this.siqnupForm.valid) {
+      console.log(this.siqnupForm.value, 'siqnupform###');
+      this.errmsgshow = false;
+    } else {
+      this.errmsgshow = true;
+      this.errmsg = 'All Fields Required !!!';
+    }
   }
-
 }
